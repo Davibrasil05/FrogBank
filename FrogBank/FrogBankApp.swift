a02373003165dfdc5eaa8d517a756fbd0cb9b9ab
Davi Brasi <davi.brasil2005@gmail.com>
2026-06-26 16:32:42 -0400

    Initial Commit

---
+++ FrogBank/FrogBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrogBankApp.swift
+//  FrogBank
+//
+//  Created by Davi Sobreira Brasil on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FrogBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
